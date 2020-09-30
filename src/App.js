0139ebe4c4faffbeb767887047033b714f2d639b
Conor Barnes <conorbarnes93@gmail.com>
2020-09-30 16:20:50 -0700

    partially get through other functions and clickarray. May have to reconceptualize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
 import './style/App.scss';
 
-function App() {
-  const [score, setScore] = useState(0)
-  const [highScore, setHighScore] = useState(0)
 
+function App() {
+  const fakeURLs = ["https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/"]
+
+  const [highScore, setHighScore] = useState(0)
+  const [score, setScore] = useState(0)
+
+  //
+  const [clickArray, setClickArray] = useState(new Array(fakeURLs.length).fill(false))
+  console.log(clickArray)
+  
   const onMiss = () => {
     setHighScore(score)
     setScore(0)
+  }
+
+  const onHit = (i) {
+    const newArray = clickArray.map((entry, index) => index === i? ...)
   }
 
   return (

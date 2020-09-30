94095c9654550bb0aafb792b2052e5186c87882c
Conor Barnes <conorbarnes93@gmail.com>
2020-09-30 16:12:15 -0700

    initiate project, install sass, create basic states

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './style/App.scss';
 
 function App() {
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+
+  const onMiss = () => {
+    setHighScore(score)
+    setScore(0)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{score}</p>
+      <p>{highScore}</p>
     </div>
   );
 }

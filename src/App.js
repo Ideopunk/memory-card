08828bdbe280b58a605dd41988c171ee8c7dc95c
Conor Barnes <conorbarnes93@gmail.com>
2020-10-04 12:15:29 -0700

    Initial card render is randomized

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./style/App.scss";
 
 function App() {
+  const shuffleArray = (array) => {
+		let newArray = Object.assign(array);
+		for (var i = newArray.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = newArray[i];
+			newArray[i] = newArray[j];
+			newArray[j] = temp;
+		}
+		return newArray;
+  };
+  
+
 	const fakeURLs = [
 		"https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/",
     "https://vignette.wikia.nocookie.net/lotr/images/f/fb/Cirdan_by_moumou38.jpg/revision/latest/top-crop/width/360/height/450?cb=20121226111023",
     "https://vignette.wikia.nocookie.net/deathbattlefanon/images/2/2a/The_Lord_of_The_Rings_-_Gandalf_The_Grey.png/revision/latest/scale-to-width-down/340?cb=20160321085616"
 	];
 
-	const [urlArray, setURLArray] = useState(
+	const [urlArray, setURLArray] = useState(shuffleArray(
 		fakeURLs.map((fakeURL) => {
 			return { url: fakeURL, clicked: false };
-		})
+		}))
 	);
 	const [highScore, setHighScore] = useState(0);
 	const [score, setScore] = useState(0);
@@ -35,16 +47,7 @@ function App() {
 		setScore(0);
 	};
 
-	const shuffleArray = (array) => {
-		let newArray = Object.assign(array);
-		for (var i = newArray.length - 1; i > 0; i--) {
-			var j = Math.floor(Math.random() * (i + 1));
-			var temp = newArray[i];
-			newArray[i] = newArray[j];
-			newArray[j] = temp;
-		}
-		return newArray;
-	};
+
 
 	const onHit = (url) => {
     console.log(`onHit, ${url}`);
@@ -73,8 +76,6 @@ function App() {
 			<img className="entrant-img" src={url.url} alt="entrant" />
 		</div>
 	));
-
-  useEffect(() => {setURLArray(shuffleArray(urlArray))}, [urlArray]);
 
 	return (
 		<div className="App">

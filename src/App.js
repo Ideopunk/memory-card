abb5523020ea75817f3502b879558da815c811c5
Conor Barnes <conorbarnes93@gmail.com>
2020-10-04 11:31:56 -0700

    Tweak onHit logic so high score doesn't keep resetting, fix object-fit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './style/App.scss';
 
 
 function App() {
-  const fakeURLs = ["https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/"]
+  const fakeURLs = ["https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/", "https://vignette.wikia.nocookie.net/lotr/images/f/fb/Cirdan_by_moumou38.jpg/revision/latest/top-crop/width/360/height/450?cb=20121226111023"]
 
   const [highScore, setHighScore] = useState(0)
   const [score, setScore] = useState(0)
@@ -13,16 +13,25 @@ function App() {
   const [clickArray, setClickArray] = useState(JSON.parse(JSON.stringify(initialArray)))
   console.log(clickArray)
   
+  const checkHighScore = () => {
+    if (highScore > score + 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   const onMiss = () => {
     setHighScore(score)
     setScore(0)
   }
 
   const onHit = (i) => {
+    console.log(`onHit, ${i}`)
     const newArray = [...clickArray];
     if (newArray[i] === false) {
       newArray[i] = true
-      setHighScore(score + 1)
+      checkHighScore()? setHighScore(highScore): setHighScore(score + 1)
       setScore(score + 1)
       setClickArray(newArray)
     } else {
@@ -31,13 +40,15 @@ function App() {
     }
   }
 
-  const mappedURLs = fakeURLs.map(url => <div className="card"><img className="entrant-img" src={url} alt="entrant" /></div>)
+  const mappedURLs = fakeURLs.map((url, index) => <div className="card" value={index} onClick={() => onHit(index)}><img className="entrant-img" src={url} alt="entrant" /></div>)
 
   return (
     <div className="App">
       <p>{score}</p>
       <p>{highScore}</p>
-      {mappedURLs}
+      <div className="game">
+        {mappedURLs}
+      </div>
     </div>
   );
 }

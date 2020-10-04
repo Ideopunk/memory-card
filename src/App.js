0cac104e2e74a3de7acc6fbc5a262cc5d7a1df43
Conor Barnes <conorbarnes93@gmail.com>
2020-10-04 12:12:53 -0700

    Fix onMiss logic so high score doesn't go down

--- src/App.js
+++ src/App.js
@@ -1,56 +1,88 @@
-import React, {useState} from 'react';
-import './style/App.scss';
-
+import React, { useState, useEffect } from "react";
+import "./style/App.scss";
 
 function App() {
-  const fakeURLs = ["https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/", "https://vignette.wikia.nocookie.net/lotr/images/f/fb/Cirdan_by_moumou38.jpg/revision/latest/top-crop/width/360/height/450?cb=20121226111023"]
+	const fakeURLs = [
+		"https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/",
+    "https://vignette.wikia.nocookie.net/lotr/images/f/fb/Cirdan_by_moumou38.jpg/revision/latest/top-crop/width/360/height/450?cb=20121226111023",
+    "https://vignette.wikia.nocookie.net/deathbattlefanon/images/2/2a/The_Lord_of_The_Rings_-_Gandalf_The_Grey.png/revision/latest/scale-to-width-down/340?cb=20160321085616"
+	];
 
-  const [highScore, setHighScore] = useState(0)
-  const [score, setScore] = useState(0)
+	const [urlArray, setURLArray] = useState(
+		fakeURLs.map((fakeURL) => {
+			return { url: fakeURL, clicked: false };
+		})
+	);
+	const [highScore, setHighScore] = useState(0);
+	const [score, setScore] = useState(0);
 
-  //
-  const initialArray = new Array(fakeURLs.length).fill(false)
-  const [clickArray, setClickArray] = useState(JSON.parse(JSON.stringify(initialArray)))
-  console.log(clickArray)
-  
-  const checkHighScore = () => {
-    if (highScore > score + 1) {
-      return true;
-    } else {
-      return false;
+	//
+
+	const checkHighScore = () => {
+		if (highScore > score + 1) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const onMiss = () => {
+    setURLArray(urlArray.map(entry => {
+      entry.clicked = false;
+      return entry;
+    }))
+		setHighScore((highScore > score? highScore: score));
+		setScore(0);
+	};
+
+	const shuffleArray = (array) => {
+		let newArray = Object.assign(array);
+		for (var i = newArray.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = newArray[i];
+			newArray[i] = newArray[j];
+			newArray[j] = temp;
+		}
+		return newArray;
+	};
+
+	const onHit = (url) => {
+    console.log(`onHit, ${url}`);
+    
+		if (url.clicked === false) {
+			setURLArray((urlArray) =>
+				urlArray.map((entry) => {
+					if (entry.url === url.url) {
+            entry.clicked = true
+          }
+          return entry;
+				})
+			);
+
+			checkHighScore() ? setHighScore(highScore) : setHighScore(score + 1);
+			setScore(score + 1);
+
+		} else {
+			onMiss();
     }
-  }
+    setURLArray(shuffleArray(urlArray))
+	};
 
-  const onMiss = () => {
-    setHighScore(score)
-    setScore(0)
-  }
+	const mappedURLs = urlArray.map((url, index) => (
+		<div className="card" key={index} value={url.clicked} onClick={() => onHit(url)}>
+			<img className="entrant-img" src={url.url} alt="entrant" />
+		</div>
+	));
 
-  const onHit = (i) => {
-    console.log(`onHit, ${i}`)
-    const newArray = [...clickArray];
-    if (newArray[i] === false) {
-      newArray[i] = true
-      checkHighScore()? setHighScore(highScore): setHighScore(score + 1)
-      setScore(score + 1)
-      setClickArray(newArray)
-    } else {
-      onMiss()
-      setClickArray(JSON.parse(JSON.stringify(initialArray)))
-    }
-  }
+  useEffect(() => {setURLArray(shuffleArray(urlArray))}, [urlArray]);
 
-  const mappedURLs = fakeURLs.map((url, index) => <div className="card" value={index} onClick={() => onHit(index)}><img className="entrant-img" src={url} alt="entrant" /></div>)
-
-  return (
-    <div className="App">
-      <p>{score}</p>
-      <p>{highScore}</p>
-      <div className="game">
-        {mappedURLs}
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>{score}</p>
+			<p>{highScore}</p>
+			<div className="game">{mappedURLs}</div>
+		</div>
+	);
 }
 
 export default App;

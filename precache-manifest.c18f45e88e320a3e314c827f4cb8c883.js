f0c1a39f721049de73128eca100dcf0cb28fdaec
Conor Barnes <conorbarnes93@gmail.com>
2020-10-05 07:55:13 -0700

    Updates

---
+++ precache-manifest.c18f45e88e320a3e314c827f4cb8c883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a286dda8cea576d6ccdb6e64447042",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "f079f543ae51b7a2f7d8",
+    "url": "/memory-card/static/css/main.dde54c57.chunk.css"
+  },
+  {
+    "revision": "496d03f2cdd31992348a",
+    "url": "/memory-card/static/js/2.eb837fd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card/static/js/2.eb837fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f079f543ae51b7a2f7d8",
+    "url": "/memory-card/static/js/main.41e2c1d3.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  }
+]);

ae63ae9037281ab32800e09a5efbfc1190b0ad6c
Conor Barnes <conorbarnes93@gmail.com>
2020-10-04 18:13:55 -0700

    Begin changing over to color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./style/App.scss";
 
 function App() {
-  const shuffleArray = (array) => {
+	const shuffleArray = (array) => {
 		let newArray = Object.assign(array);
 		for (var i = newArray.length - 1; i > 0; i--) {
 			var j = Math.floor(Math.random() * (i + 1));
@@ -11,19 +11,27 @@ function App() {
 			newArray[j] = temp;
 		}
 		return newArray;
-  };
-  
+	};
 
-	const fakeURLs = [
-		"https://vignette.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/",
-    "https://vignette.wikia.nocookie.net/lotr/images/f/fb/Cirdan_by_moumou38.jpg/revision/latest/top-crop/width/360/height/450?cb=20121226111023",
-    "https://vignette.wikia.nocookie.net/deathbattlefanon/images/2/2a/The_Lord_of_The_Rings_-_Gandalf_The_Grey.png/revision/latest/scale-to-width-down/340?cb=20160321085616"
+	const colors = [
+		{ rgb: "(62, 180, 137)", name: "Mint" },
+		{ rgb: "(208, 240, 192)", name: "Tea" },
+		{ rgb: "(128, 128, 0)", name: "Olive" },
+		{ rgb: "(1, 121, 111)", name: "Pine" },
+		{ rgb: "(138, 154, 91)", name: "Moss" },
+		{ rgb: "(192, 255, 0)", name: "Lime" },
+		{ rgb: "(113, 188, 120)", name: "Fern" },
+		{ rgb: "(86, 130, 3)", name: "Avocado" },
+    { rgb: "(135, 169, 107)", name: "Asparagus" },
+    {rgb: "(75, 111, 68)", name: "Artichoke"}
 	];
 
-	const [urlArray, setURLArray] = useState(shuffleArray(
-		fakeURLs.map((fakeURL) => {
-			return { url: fakeURL, clicked: false };
-		}))
+	const [urlArray, setURLArray] = useState(
+		shuffleArray(
+			colors.map((fakeURL) => {
+				return { url: fakeURL, clicked: false };
+			})
+		)
 	);
 	const [highScore, setHighScore] = useState(0);
 	const [score, setScore] = useState(0);
@@ -39,36 +47,35 @@ function App() {
 	};
 
 	const onMiss = () => {
-    setURLArray(urlArray.map(entry => {
-      entry.clicked = false;
-      return entry;
-    }))
-		setHighScore((highScore > score? highScore: score));
+		setURLArray(
+			urlArray.map((entry) => {
+				entry.clicked = false;
+				return entry;
+			})
+		);
+		setHighScore(highScore > score ? highScore : score);
 		setScore(0);
 	};
 
-
-
 	const onHit = (url) => {
-    console.log(`onHit, ${url}`);
-    
+		console.log(`onHit, ${url}`);
+
 		if (url.clicked === false) {
 			setURLArray((urlArray) =>
 				urlArray.map((entry) => {
 					if (entry.url === url.url) {
-            entry.clicked = true
-          }
-          return entry;
+						entry.clicked = true;
+					}
+					return entry;
 				})
 			);
 
 			checkHighScore() ? setHighScore(highScore) : setHighScore(score + 1);
 			setScore(score + 1);
-
 		} else {
 			onMiss();
-    }
-    setURLArray(shuffleArray(urlArray))
+		}
+		setURLArray(shuffleArray(urlArray));
 	};
 
 	const mappedURLs = urlArray.map((url, index) => (
@@ -79,8 +86,18 @@ function App() {
 
 	return (
 		<div className="App">
-			<p>{score}</p>
-			<p>{highScore}</p>
+			<div>
+				<header className="header">
+					<h1>Memory Card</h1>
+				</header>
+				<div className="guide">
+					<p>
+						Get a high score by clicking each image without clicking any more than once
+					</p>
+					<p>Score: {score}</p>
+					<p>High score: {highScore}</p>
+				</div>
+			</div>
 			<div className="game">{mappedURLs}</div>
 		</div>
 	);
